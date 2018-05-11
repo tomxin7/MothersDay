7e7b46c95b487454739e4288db2ba22c08eb09f9
tomxin <221360@nd.com>
2018-05-11 18:39:38 +0800

    修改了目录结构，新增diy页面

--- js/jscex-jit.js
+++ js/jscex-jit.js
@@ -1380,7 +1380,7 @@
             init(root);
         }
     } else if (isAmd) {
-        define("jscex-jit", ["jscex-parser"], function (parser) {
+        define("jscex-jit", ["js/jscex-parser"], function (parser) {
             return {
                 init: function (root) {
                     if (!root.modules["parser"]) {
